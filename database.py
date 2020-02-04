2fb96c1534fc605d9c33b35f1bd7a14086a8562e
Jessica Nash <janash@vt.edu>
2020-02-04 14:32:25 -0500

    make journal objects in advance so that additional papers from same journal can be added to db

--- database.py
+++ database.py
@@ -22,18 +22,22 @@ session = Session()
 
 Base.metadata.create_all(engine)
 
+jchem_phys = Journal(name='J. Chem. Phys', publisher='AIP')
+jcim = Journal(name='J. Chem. Inf. Model', publisher='ACS')
+
 # Add a paper to the DB
-molssi_paper = Paper(DOI='10.1063/1.5052551', paper_title='Perspective: Computational chemistry software and its advancement as illustrated through three grand challenge cases for molecular science', journal=Journal(name='J. Chem. Phys'), publication_year=2018, authors='Anna Krylov, Theresa L. Windus, Taylor Barnes, Eliseo Marin-Rimoldi, Jessica A. Nash, Benjamin Pritchard, Daniel G.A. Smith, Doaa Altarawy, Paul Saxe, Cecilia Clementi, T. Daniel Crawford, Robert J. Harrison, Shantenu Jha, Vijay S. Pande, Teresa Head-Gordon')
+molssi_paper = Paper(DOI='10.1063/1.5052551', paper_title='Perspective: Computational chemistry software and its advancement as illustrated through three grand challenge cases for molecular science', journal=jchem_phys, publication_year=2018, authors='Anna Krylov, Theresa L. Windus, Taylor Barnes, Eliseo Marin-Rimoldi, Jessica A. Nash, Benjamin Pritchard, Daniel G.A. Smith, Doaa Altarawy, Paul Saxe, Cecilia Clementi, T. Daniel Crawford, Robert J. Harrison, Shantenu Jha, Vijay S. Pande, Teresa Head-Gordon')
 
 # Add another paper
-bse_paper = Paper(DOI='10.1021/acs.jcim.9b00725', paper_title='New Basis Set Exchange: An Open, Up-to-Date Resource for the Molecular Sciences Community', journal=Journal(name='J Chem. Inf. Model'), publication_year=2019, authors='Benjamin P. Pritchard, Doaa Altarawy, Brett Didier, Tara D. Gibson, Theresa L. Windus')
+bse_paper = Paper(DOI='10.1021/acs.jcim.9b00725', paper_title='New Basis Set Exchange: An Open, Up-to-Date Resource for the Molecular Sciences Community', journal=jcim, publication_year=2019, authors='Benjamin P. Pritchard, Doaa Altarawy, Brett Didier, Tara D. Gibson, Theresa L. Windus')
 
-# Add another paper for J Phys Chem
-
+# Add another paper for J Chem Phys
+crawford_paper = Paper(DOI='10.1063/1.2137323', paper_title='Sources of error in electronic structure calculations on samll chemical systems', journal=jchem_phys, publication_year=2006, authors='David Feller, Kirk A. Peterson, T. Daniel Crawford')
 
 # Commit to DB
 session.add(molssi_paper)
 session.add(bse_paper)
+session.add(crawford_paper)
 session.commit()
 
 # Retrieve from DB
@@ -41,10 +45,5 @@ session.commit()
 #print(my_paper.paper_title)
 
 all_papers = session.query(Paper).all()
-
-by_journal = session.query(Journal).filter(Journal.name == 'J. Chem. Phys').first()
-
-#print(by_journal.papers)
-
 
 session.close()
